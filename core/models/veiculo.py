2c550f4a2c943386b3da861b03cacf50f470ca20
Murilo da Cruz Souza <131203246+MuriloDaCruz@users.noreply.github.com>
2025-05-01 16:56:16 -0300

    feat: Criação veiculo na models

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -6,7 +6,7 @@ from .acessorio import Acessorio
 
 class Veiculo(models.Model):
     ano = models.IntegerField(null=True, blank=True, default=0)
-    preco = models.DecimalField(max_length=10, decimal_places=2, null=True, blank=True, default=0)
+    preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, default=0)
     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT)
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT)
     acessorio = models.ManyToManyField(Acessorio)
