608f346a7f9852f49cff8fac423de6cbfb387146
Murilo da Cruz Souza <131203246+MuriloDaCruz@users.noreply.github.com>
2025-05-01 16:51:21 -0300

    feat: Criação veiculo na models

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -4,7 +4,7 @@ from .modelo import Modelo
 from .cor import Cor
 from .acessorio import Acessorio
 
-class Veiculo(models.Model)
+class Veiculo(models.Model):
     ano = models.IntegerField(null=True, blank=True, default=0)
     preco = models.DecimalField(max_length=10, decimal_places=2, null=True, blank=True, default=0)
     modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT)
