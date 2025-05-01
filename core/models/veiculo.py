d0949b6492a6775f760a06e0d17d9190d9d2f776
Murilo da Cruz Souza <131203246+MuriloDaCruz@users.noreply.github.com>
2025-05-01 14:57:01 -0300

    feat: Criação veiculo na models

---
+++ core/models/veiculo.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+from .modelo import Modelo
+from .cor import Cor
+from .acessorio import Acessorio
+
+class Veiculo(models.Model)
+    ano = models.IntegerField(null=True, blank=True, default=0)
+    preco = models.DecimalField(max_length=10, decimal_places=2, null=True, blank=True, default=0)
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT)
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT)
+    acessorio = models.ManyToManyField(Acessorio)
+
+    def __str__(self):
+      return f"({self.id}) {self.modelo} - {self.cor} - {self.ano}"
